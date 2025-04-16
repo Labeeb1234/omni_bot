c699d2dad65f060bc1be505c1f4f5452a9a5a694
Labeeb Nassar <labeebranassar@gmail.com>
2025-04-16 17:48:49 +0530

    test code for testing registered gym env

--- test.py
+++ test.py
@@ -1,21 +1,25 @@
 import numpy as np
-from omni_bot.env_gym import OmniBotEnv
+import omni_bot.envs
+import gymnasium as gym
 import time  # For adding a slight delay
 
-env = OmniBotEnv(render_mode="human")  # Initialize environment
-done = False
-state, _ = env.reset()  # Get initial state
-print(state)
-count = 0
-while count < 1000:
-    action = np.array([
-        [1.0, 1.0, 1.0],
-    ])  # Rotate in place
-    new_state, reward, dones, _ = env.step(actions=action)  # Take action
-    print(f"State: {new_state}, Reward: {reward}, dones: {dones}")  # Debug output
-    env.render()  # Update visualization
+env = gym.make('OmniBot-v0', render_mode='human')
 
+done = False
+state, _ = env.reset()
+count = 0
+
+while not done:
+    action = 0
+    new_state, reward, done, trunc, _ = env.step(action=action)
+    print(f"State: {new_state}, Reward: {reward}, dones: {done}")
+    env.render()
     count += 1
 
-env.save_trajectory()
-env.load_trajectory()
+env.close()
+
+
+
+
+
+
